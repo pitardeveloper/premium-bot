0c18b84b20d0d208da1703c5440b95715eca4bfb
pitardeveloper <37588350+pitardeveloper@users.noreply.github.com>
2018-03-20 21:38:51 +0300

    Create perm.js

---
+++ perm.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('ready',  () => {
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+  console.log('CREATED BY : PITAR HIX');
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log(`Logged in as  * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log('⚙            https://discord.gg/2yCdFZ | سيرفر الدعم');
+  console.log('⚙            PITAR : ibarmoda');
+client.user.setStatus("dnd");
+});
+                                                                                                                                                                                              var prefix = "!";
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command === "Bb") {
+    let numArray = args.map(n=> parseInt(n));
+    let total = numArray.reduce( (p, c) => p+c);
+
+    message.channel.sendMessage(total).catch(console.error);
+  }
+  
+
+  });
+
+// playing
+client.on('ready', () => {
+                                                                                                                                                                                                                                                                              client.user.setGame(`The Premium Bot | Created By PITAR`,'https://www.twitch.tv/v5bz');
+});
+
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
